5268b18d5bd155a9898f4d40c26a36000169ea04
Nina Utrata <ninamichelleutrata@gmail.com>
2017-12-04 19:12:45 +0100

    Setters and Getters jupi!

--- src/sample/Clock.java
+++ src/sample/Clock.java
@@ -7,38 +7,64 @@ public class Clock {
     private int currentStep = 0; // current step number
     private int currentPulse = 0; // current pulse number
 
+    //zmienne zmieniane w interfejsie
+    private int currentTempo= 0;
+    private int currentSkip= 0;
+    //gateMode - zwraca wartosci 1-4 z kazdego slidera tak jak na obrazku
+    //1 -LONGG - one long sound lasting N pulses
+    //2 -MULTIPLE - N short sounds
+    //3 -SINGLE - one short sound and N-1 pulses-long pause
+    //4 -PAUSE - N pulses-long pause
+    private int[] gateMode;
+    //Skip - zwraca ktore z stepow mamy pominac(wartsoc true)
+    private Boolean[] Skip;
+    private Boolean Play;
+    //PulseCount - zwraca wartosci w przedziale 1-8 z kazdego slidera tak jak na obrazku
+    private int[] PulseCount;
+
+    public Clock(){
+        Skip= new Boolean[8];
+    }
     //szalenstwo
-    public void setTempo(int tempo){
-    currentPulse=tempo;
-    }
+    public void setTempo(int tempo){ currentTempo=tempo; }
 
-    public void update(boolean play){
-        while(UI.getPlay()){
-            UI.updateUI();
+    public void setStep(int step){ currentStep=step;}
 
-            if(timeCount >= UI.getTempo()*TIME_CONSTANT){
-                timeCount = 0;
-            }
+    public void setPlay(Boolean play){ Play=play;  System.out.println(play);}
 
-            if(timeCount == 0){
-                if(currentPulse >= UI.getPulseCount(currentStep)) {
-                    currentPulse = 0;
-                    currentStep = (currentStep + 1) % UI.getSteps();
-                    while (UI.getSkip(currentStep)){
-                        currentStep = (currentStep + 1) % UI.getSteps();
-                    }
-                }
-                else{
-                    currentPulse++;
-                }
-                Playback.play(currentStep, currentPulse);
-            }
+    public void setGateMode(int [] gatemode){ gateMode=gatemode; }
 
-            lastTime = currentTime;
-            currentTime = System.currentTimeMillis();
-            timeCount += currentTime - lastTime;
-        }
-    }
+    public void setSkip(Boolean [] skip){ Skip=skip; }
+
+    public void setPulseCount(int [] pulse){PulseCount=pulse;}
+
+//    public void update(boolean play){
+//        while(UI.getPlay()){
+//            UI.updateUI();
+//
+//            if(timeCount >= UI.getTempo()*TIME_CONSTANT){
+//                timeCount = 0;
+//            }
+//
+//            if(timeCount == 0){
+//                if(currentPulse >= UI.getPulseCount(currentStep)) {
+//                    currentPulse = 0;
+//                    currentStep = (currentStep + 1) % UI.getSteps();
+//                    while (UI.getSkip(currentStep)){
+//                        currentStep = (currentStep + 1) % UI.getSteps();
+//                    }
+//                }
+//                else{
+//                    currentPulse++;
+//                }
+//                Playback.play(currentStep, currentPulse);
+//            }
+//
+//            lastTime = currentTime;
+//            currentTime = System.currentTimeMillis();
+//            timeCount += currentTime - lastTime;
+//        }
+//    }
 }
 
 
--- src/sample/Sequencer.java
+++ src/sample/Sequencer.java
@@ -1,55 +1,73 @@
 package sample;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Slider;
 
 public class Sequencer {
-    //szaleństwo
     private Clock clock;
-    private UI ui;
-    public Slider tempo;
-    public Slider steps;
-    public CheckBox skip1,skip2,skip3,skip4,skip5,skip6,skip7,skip8,longg,multiple,single,pause;
-    public ComboBox instrument;
+    private Playback playback;
+    @FXML
+    private CheckBox skip1,skip2,skip3,skip4,skip5,skip6,skip7,skip8,play;
+    @FXML
+    private ComboBox instrument;
+    @FXML
+    private Slider PulseCount1,PulseCount2,PulseCount3,PulseCount4,PulseCount5,PulseCount6,PulseCount7,PulseCount8,
+            Gate1,Gate2,Gate3,Gate4,Gate5,Gate6,Gate7,Gate8,steps,tempo;
+
 
     public Sequencer(){
-        ui=new UI();
         clock=new Clock();
+        playback=new Playback();
     }
 
-    public void changetempo()
-    {
-        //szaleństwo
-         clock.setTempo((int)tempo.getValue());
-       ui.tempo=(int)tempo.getValue();
-    }
+    public void changeplay(){ clock.setPlay(!play.isSelected());}
 
-    public void changesteps(){
-        ui.steps=(int)steps.getValue();
-    }
+    public void changeinstrument(){playback.setInstrument(instrument.getValue().toString());}
+
+    public void changetempo() { clock.setTempo((int)tempo.getValue()); }
+
+    public void changesteps(){ clock.setStep((int)steps.getValue()); }
+
     public void changeskip(){
-
-        ui.skip[0] = skip1.isSelected();
-        ui.skip[1] = skip2.isSelected();
-        ui.skip[2] = skip3.isSelected();
-        ui.skip[3] = skip4.isSelected();
-        ui.skip[4] = skip5.isSelected();
-        ui.skip[5] = skip6.isSelected();
-        ui.skip[6] = skip7.isSelected();
-        ui.skip[7] = skip8.isSelected();
+        clock.setSkip(new Boolean[]{
+            skip1.isSelected(),
+            skip2.isSelected(),
+            skip3.isSelected(),
+            skip4.isSelected(),
+            skip5.isSelected(),
+            skip6.isSelected(),
+            skip7.isSelected(),
+            skip8.isSelected()
+        });
     }
 
-    public void changeinstrument(){
-        ui.instrumentNumber=(int)instrument.getValue();
+    public void changepulsecounts(){
+        clock.setPulseCount(new int[]{
+            (int)PulseCount1.getValue(),
+            (int)PulseCount2.getValue(),
+            (int)PulseCount3.getValue(),
+            (int)PulseCount4.getValue(),
+            (int)PulseCount5.getValue(),
+            (int)PulseCount6.getValue(),
+            (int)PulseCount7.getValue(),
+            (int)PulseCount8.getValue(),
+
+        });
     }
 
     public void changemode(){
+        clock.setGateMode(new int[]{
+            (int)Gate1.getValue(),
+            (int)Gate2.getValue(),
+            (int)Gate3.getValue(),
+            (int)Gate4.getValue(),
+            (int)Gate5.getValue(),
+            (int)Gate6.getValue(),
+            (int)Gate7.getValue(),
+            (int)Gate8.getValue(),
 
-        ui.gateMode[0] = longg.isSelected();
-        ui.gateMode[1] = multiple.isSelected();
-        ui.gateMode[2] = single.isSelected();
-        ui.gateMode[3] = pause.isSelected();
-
+        });
     }
 }
