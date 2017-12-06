6b3350b869a9463c738abf3ab690ccc852f1ff5a
Karol Świerczek <karolswierczek5@gmail.com>
2017-12-06 12:05:11 +0100

    Minor fixes in clock

--- src/sample/Clock.java
+++ src/sample/Clock.java
@@ -5,11 +5,15 @@ public class Clock extends Thread{
     private Object lock;
     private boolean exit = false;
     private static float TIME_CONSTANT = 200000000.0f; // converts time from tempo to ms
+    private static float MAX_TEMPO = 8;
     private long lastTime = 0; // time of last while iteration
     private long currentTime = 0; // time of current iteration
     private float timeCount = 0; // time from last pulse
     private int currentStep = 0; // current step number
     private int currentPulse = 0; // current pulse number
+
+    //for testing purposes only!
+    private static String[] ticks = {"bip", "bop"};
 
     public Clock(Settings s, Object l) {
         settings = s;
@@ -29,25 +33,26 @@ public class Clock extends Thread{
                     }
                 }
             }
+            currentTime = System.nanoTime();
             while (settings.getPlay()) {
-                if (timeCount >= settings.getTempo() * TIME_CONSTANT) {
+                if (timeCount >= MAX_TEMPO/(settings.getTempo()+1) * TIME_CONSTANT) {
 
                     timeCount = 0;
                 }
 
                 if (timeCount == 0) {
-                    if (currentPulse >= settings.getPulseCount()[currentStep]) {
+                    if (currentPulse >= settings.getPulseCount()[currentStep]-1) {
                         currentPulse = 0;
                         currentStep = (currentStep + 1) % settings.getSteps();
                         while (settings.getSkip()[currentStep]) {
                             currentStep = (currentStep + 1) % settings.getSteps();
                         }
-                        System.out.println(currentStep);
+                        System.out.println(currentStep+1);
                     } else {
                         currentPulse++;
-                        System.out.println(" bip");
-                        //playback.play(currentStep, currentPulse);
                     }
+                    System.out.println(ticks[currentPulse%2]);
+                    //playback.play(currentStep, currentPulse);
                 }
 
                 lastTime = currentTime;
